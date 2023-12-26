782d208a9a7fd200afe47e3b7c3ce774124048df
aerlingsson <arnar.h.erlingsson@gmail.com>
2023-12-26 14:14:35 +0100

    Init

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+open System.Net.Http
+
+try
+  let modpackUrl = "http://localhost:5257/modpack"
+  let userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+  let userDesktopDir = Path.GetFullPath($"{userDir}/Desktop")
+  let modDirName = "flurry97_private_server_mods"
+
+  let httpClient = new HttpClient()
+
+  printfn $"Downloading modpack info from {modpackUrl}..."
+  let modpackResponse = httpClient.GetAsync(modpackUrl) |> Async.AwaitTask |> Async.RunSynchronously
+
+  printfn "Reading modpack info..."
+  let modpackBytes = modpackResponse.Content.ReadAsByteArrayAsync() |> Async.AwaitTask |> Async.RunSynchronously
+
+  printfn $"Writing modpack info to {userDesktopDir}\{modDirName}.zip..."
+  File.WriteAllBytes($"{userDesktopDir}/{modDirName}.zip", modpackBytes)
+
+  printfn "Press any key to exit"
+  Console.ReadKey() |> ignore
+with ex ->
+  printfn $"Error: {ex.Message}"
+  Console.ReadKey() |> ignore
