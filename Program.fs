ea41abb88a29de27de421984f150e70b8a97c7e8
aerlingsson <arnar.h.erlingsson@gmail.com>
2023-12-26 17:32:29 +0100

    Just use user dir instead of desktop dir

--- Program.fs
+++ Program.fs
@@ -2,10 +2,12 @@
 open System.IO
 open System.Net.Http
 
+// Publish command: dotnet publish -c Release -r win10-x64
+// Exe is in bin\Release\net7.0\win10-x64\publish\MinecraftPrivateServerModsUpdater.exe
 try
   let modpackUrl = "http://78.156.125.32/modpack"
   let userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-  let userDesktopDir = Path.GetFullPath($"{userDir}/Desktop")
+  let userDirPath = Path.GetFullPath($"{userDir}")
   let modDirName = "flurry97_private_server_mods"
 
   let httpClient = new HttpClient()
@@ -16,8 +18,8 @@ try
   printfn "Reading modpack info..."
   let modpackBytes = modpackResponse.Content.ReadAsByteArrayAsync() |> Async.AwaitTask |> Async.RunSynchronously
 
-  printfn $"Writing modpack info to {userDesktopDir}\{modDirName}.zip..."
-  File.WriteAllBytes($"{userDesktopDir}/{modDirName}.zip", modpackBytes)
+  printfn $"Writing modpack info to {userDirPath}\{modDirName}.zip..."
+  File.WriteAllBytes($"{userDirPath}\{modDirName}.zip", modpackBytes)
 
   printfn "Press any key to exit"
   Console.ReadKey() |> ignore
